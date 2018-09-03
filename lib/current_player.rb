edb2bf54189054948f19278f6986fb440cd9cd12
Rudy Ravelin <rudy@noukod.com>
2018-09-03 19:38:18 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  i = 0
+  count = 0
+  while i <= 9
+    if board[i] == "X" || board[i] == "O"
+      count += 1
+    end
+    i += 1
+  end
+  count
+
+end
+turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
+def current_player(board)
+
+  turn_count(board).even? ? "X" : "O"
+end
